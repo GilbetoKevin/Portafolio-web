e5f947ac782f5c34b4df14c7aab3d0261cbb3bf8
GilbetoKevin <gikeva@gmail.com>
2024-08-31 02:26:11 -0600

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.getElementById('contact-button').addEventListener('click', function() {
+    window.location.href = '#contact';
+});
+document.getElementById('emailButton').addEventListener('click', function () {
+    window.location.href = 'mailto:gikevara@gmail.com?subject=Consulta&body=Hola%20que%20puedo%20ayudarte';
+});
+document.getElementById('callButton').addEventListener('click', function() {
+    window.location.href = 'tel:+528311588761';
+});
+document.getElementById('locationButton').addEventListener('click', function() {
+    window.location.href = 'https://www.google.com/maps/place/89928+El+Mante,+Tamps./@22.5223021,-98.8016507,15z/data=!3m1!4b1!4m6!3m5!1s0x8678990008290953:0x2add78779efc8e41!8m2!3d22.522283!4d-98.791351!16s%2Fg%2F12xqvf2py?entry=ttu&g_ep=EgoyMDI0MDgyOC4wIKXMDSoASAFQAw%3D%3D';
+});
+document.getElementById('view-more-btn').addEventListener('click', function() {
+    window.location.href = 'https://github.com/GilbetoKevin/BloomingSpirit';
+});
